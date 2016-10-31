9ac6aff55a1f056147da2f196cdfec239af7ecf6
Antoine NOSAL <antoine.nosal@gmail.com>
2016-10-31 11:12:52 +0100

    modifie certaines classe, ajoute des composants jade

--- src/main/java/fr/maxinosal/jade/HelloWorldAgent.java
+++ src/main/java/fr/maxinosal/jade/HelloWorldAgent.java
@@ -1,7 +1,12 @@
-package fr.maxinosal;
+package fr.maxinosal.jade;
 
 import jade.core.Agent;
 
+/**
+ * java -cp "jade\lib\jade.jar;target\classes" jade.Boot -gui
+ * @author Maxime BLAISE
+ * @author Antoine NOSAL
+ */
 public class HelloWorldAgent extends Agent {
 
     protected void setUp() {
